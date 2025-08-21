7aa7d5ea8fba73b751b86040c6cb03a483f1dff8
ekarga <egekaanduman@gmail.com>
2025-08-20 19:00:44 -0700

    chore: alias editor binary to Orca

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 # for mac os
 scons platform=macos target=editor dev_build=yes vulkan=no
 
+# ensure friendly binary name
+ln -sf ./godot.macos.editor.dev.arm64 ./bin/Orca
+
 # run
-./bin/godot.macos.editor.dev.arm64
+./bin/Orca
