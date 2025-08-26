2061cdf8a234ef55b68a784fb3c6a91bec342dfd
ekarga <egekaanduman@gmail.com>
2025-08-25 20:56:57 -0700

    Update run script to use Orca symlink

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # for mac os
 scons platform=macos target=editor dev_build=yes vulkan=no
 
-# run the compiled binary directly
-./bin/orca.macos.editor.dev.arm64
+# run the compiled binary via symlink
+./bin/Orca
